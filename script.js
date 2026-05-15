65bd1390c2e499cf43dab37fa78fffe4339397db
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-15 12:40:17 +0200

    Try with async call

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 async function getChefBirthday(id) {
-  const responseRicetta = await fetch(` https://dummyjson.com/recipes/${id}`);
+  try {
+    const responseRicetta = await fetch(`https://dummyjson.com/recipes/${id}`);
 
-  const ricetta = await responseRicetta.json();
+    const ricetta = await responseRicetta.json();
 
-  const userId = ricetta.userId;
+    const userId = ricetta.userId;
 
-  const responseChefInfo = await fetch(`https://dummyjson.com/users/${userId}`);
+    const responseChefInfo = await fetch(
+      `https://dummyjson.com/users/${userId}`,
+    );
 
-  const chefInfo = await responseChefInfo.json();
+    const chefInfo = await responseChefInfo.json();
 
-  const dateOfBirthdayChef = chefInfo.birthDate;
+    const dateOfBirthdayChef = chefInfo.birthDate;
 
-  return dateOfBirthdayChef;
+    return dateOfBirthdayChef;
+  } catch (error) {
+    console.error("L'errore è", error.message);
+  }
 }
 
-getChefBirthday(1)
-  .then((birthday) => console.log('La data di nascita dello chef è:', birthday))
-  .catch((errore) => console.error(errore))
-  .finally((messaggio) => console.log('Operazione conclusa'));
+// getChefBirthday(505050)
+//   .then((birthday) => console.log('La data di nascita dello chef è:', birthday))
+//   .catch((errore) => console.error(errore.message))
+//   .finally((messaggio) => console.log('Operazione conclusa'));
+
+(async () => {
+  try {
+    const birthday = await getChefBirthday(1);
+
+    console.log('la data di nascia dello chef è:', birthday);
+  } catch (errore) {
+    throw new Error("L'errore è questo", errore);
+  }
+})();
